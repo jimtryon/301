3f053dd3b43cde3dbf68d87770a87c9c1861e178
Jim Tryon <jtryon@mail.greenriver.edu>
2014-12-06 00:00:55 -0800

    Modified code to use a struct

--- PoolPuzzle/PoolPuzzle/poolpuzzle.c
+++ PoolPuzzle/PoolPuzzle/poolpuzzle.c
@@ -5,29 +5,39 @@
 //  Created by Jim Tryon on 12/5/14.
 //  Copyright (c) 2014 Jim Tryon. All rights reserved.
 //
-// The program has two functions: one that prints out catalog information
-// and the other that prints out the label.
+// The program is an example of structs that has two functions:
+// one that prints out catalog information and the other that
+// prints out the label.
 
 #include <stdio.h>
+struct fish {
+    const char *name;
+    const char *species;
+    int teeth;
+    int age;
+};
 
 /* Print out the catalog entry */
-void catalog(const char *name, const char *species, int teeth, int age)
+void catalog(struct fish f)
 {
     printf("%s is a %s with %i teeth.  He is %i\n",
-           name, species, teeth, age);
+           f.name, f.species, f.teeth, f.age);
     
 }
 
 /* Print the label for the tank */
-void label(const char *name, const char *species, int teeth, int age)
+void label(struct fish f)
 {
     printf("Name:%s\nSpecies:%s\n%i years old, %i teeth\n",
-           name, species, age, teeth);
+           f.name, f.species, f.teeth, f.age);
 }
 
 int main(int argc, const char * argv[]) {
-    catalog("Snappy", "Pirahna", 69, 4);
-    label("Snappy", "Pirahna", 69, 4);
+    struct fish snappy = {"Snappy", "Pirahna", 4, 69};
+    catalog(snappy);
+    label(snappy);
+    
+    return 0;
 }
 
 
